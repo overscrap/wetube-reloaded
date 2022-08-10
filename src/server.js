2b175101e16cee6aee30dfc880e9a7db2363e6aa
MOONJONG KIM <hushkmj@naver.com>
2022-08-10 18:23:59 +0900

    commit

--- src/server.js
+++ src/server.js
@@ -4,26 +4,52 @@ const PORT = 4000;
 
 const app = express();
 
+const urlLogger = (req, res, next) => {
+    console.log(`path: ${req.url}`);
+    next();
+};
+const securityLogger = (req, res, next) => {
+    const protocol = req.protocol;
+    if(protocol === "https"){
+        console.log(`protocol: secure`);
+    }else{
+        console.log(`protocol: insecure`);
+    }
+    next();
+};
+const privateMiddleware = (req, res, next) => {
+   const url = req.url;
+   if(url === "/protected"){
+        return res.send("<h1>Not Allowed</h1>");
+   }
+   console.log("Allowed, you may continue.");
+   next();
+};
+
+const timeLogger = (req, res, next) => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDay();
+    console.log(`time: ${year}.${month}.${day}`);
+    next();
+};
+
 const handleHome = (req, res) => {
-    return res.send("<h1>Home</h1>");
-}
+    return res.send("I love middlewares");
+};
 
-const handleAbout = (req, res) => {
-    return res.send("<h1>About</h1>");
-}
+const handleProtected = (req, res) => {
+    return res.send("<h1>welcome to the private lounge</h1>");
+};
 
-const handleContact = (req, res) => {
-    return res.send("<h1>Contact</h1>");
-}
-const handleLogin = (req, res) => {
-    return res.send("<h1>Login</h1>");
-}
-
+app.use(urlLogger);
+app.use(privateMiddleware);
+app.use(timeLogger);
+app.use(securityLogger);
 app.get("/", handleHome);
-app.get("/about", handleAbout);
-app.get("/contact", handleContact);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
-const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}!!!`);
+const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT} !!!`);
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
