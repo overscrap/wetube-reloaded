1c58814f683dd869c7e76373cef00e7628064b80
MOONJONG KIM <hushkmj@naver.com>
2022-08-10 18:33:13 +0900

    commit

--- src/server.js
+++ src/server.js
@@ -7,33 +7,36 @@ const app = express();
 const urlLogger = (req, res, next) => {
     console.log(`path: ${req.url}`);
     next();
-};
-const securityLogger = (req, res, next) => {
-    const protocol = req.protocol;
-    if(protocol === "https"){
-        console.log(`protocol: secure`);
-    }else{
-        console.log(`protocol: insecure`);
+  };
+  
+  const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+      return res.send("<h1>Not Allowed</h1>");
     }
+    console.log("Allowed, you may continue.");
     next();
-};
-const privateMiddleware = (req, res, next) => {
-   const url = req.url;
-   if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>");
-   }
-   console.log("Allowed, you may continue.");
-   next();
-};
-
-const timeLogger = (req, res, next) => {
+  };
+  
+  const timeLogger = (req, res, next) => {
     const date = new Date();
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDay();
     console.log(`time: ${year}.${month}.${day}`);
     next();
-};
+  };
+  
+  const securityLogger = (req, res, next) => {
+    const protocol = req.protocol;
+    if (protocol === "https") {
+      console.log(`protocol: secure`);
+    } else {
+      console.log(`protocol: insecure`);
+    }
+    next();
+  };
+  
 
 const handleHome = (req, res) => {
     return res.send("I love middlewares");
