e916412d3d2c3f4ed3f1d562dedd0344644a50b1
hushkmj <hushkmj@naver.com>
2022-08-18 23:50:16 +0900

    #6.6 More Practice part Two

---
+++ src/routers/movieRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { home, movieDetail } from "../controllers/movieController";
+
+const movieRouter = express.Router();
+
+// create the '/' route
+// create the /:id route
+// create the /add route (GET + POST)
+
+export default movieRouter;
--- src/server.js
+++ src/server.js
@@ -1,20 +1,27 @@
 import express from "express";
+import path from "path";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import movieRouter from "./routers/movieRouter";
+import { localsMiddleware } from "./middlewares";
 
 const PORT = 4000;
-console.log(process.cwd());
+
 const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.set("views", path.join(__dirname, "views"));
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/user", userRouter);
+app.use(localsMiddleware);
+app.use("/", movieRouter);
 
 const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT} !!!`);
 
