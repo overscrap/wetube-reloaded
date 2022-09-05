135cf64d37bf955bcaecc07df53aa28ceca226be
MOONJONG KIM <hushkmj@naver.com>
2022-09-05 17:46:14 +0900

    7.22 Log Out

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import fetch from "node-fetch"
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
@@ -31,10 +32,11 @@ export const postJoin = async (req, res) => {
     return res.redirect("/login");
 };
 export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" });
+
 export const postLogin = async (req, res) => {
     const { username, password } = await req.body;
     const pageTitle = "Login";
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ username,socialOnly: false });
 
     if (!user) {
         return res.status(400).render("login", {
@@ -58,7 +60,7 @@ export const postLogin = async (req, res) => {
 export const startGithubLogin = (req,res) => {
     const baseUrl = "https://github.com/login/oauth/authorize";
     const config = {
-        client_id : "d8d12032f7e15c95df78",
+        client_id : process.env.GH_CLIENT,
         allow_signup : false,
         scope : "read:user user:email",
     };
@@ -68,11 +70,79 @@ export const startGithubLogin = (req,res) => {
     return res.redirect(finalUrl);
 }
 
-export const finishGithubLogin = (req,res) => {
-    res.end();
-}
+export const finishGithubLogin = async (req,res) => {
+    const baseUrl = "https://github.com/login/oauth/access_token";
+    const config = {
+        client_id : process.env.GH_CLIENT,
+        client_secret : process.env.GH_SECRET,
+        code: req.query.code,
+    }
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+
+    const tokenRequest = await (
+        await fetch(finalUrl, {
+            method:"POST",
+            headers: {
+                Accept: "application/json"
+            }
+        })
+    ).json();
+
+    if("access_token" in tokenRequest){
+        const {access_token} = tokenRequest;
+        const apiUrl = "https://api.github.com";
+
+        const userData = await (
+            await fetch(`${apiUrl}/user`,{
+                headers: {
+                    Authorization: `token ${access_token}`
+                }
+            })
+        ).json();
+        // console.log(userData);
+
+        const emailData = await(
+            await fetch(`${apiUrl}/user/emails`,{
+                headers: {
+                    Authorization: `token ${access_token}`
+                }
+            })
+        ).json();
+        
+        const emailObj = emailData.find(
+            (email) => email.primary === true && email.verified === true
+        );
+        
+        if(!emailObj){
+            return res.redirect("/login");
+        }
+
+        let user = await User.findOne({email: emailObj.email});
+
+        if(!user){
+            user = await User.create({
+                avatarUrl:userData.avatar_url,
+                name: userData.name,
+                username: userData.login,
+                email: emailObj.email,
+                password: "",
+                socialOnly: true,
+                location: userData.location,
+            });
+        }
+        req.session.loggedIn = true;
+        req.session.user = user;
+        return res.redirect("/");
+        
+    }else{
+        return res.redirect("/login");
+    }
+};
 
 export const edit = (req, res) => res.render("editUser", { pageTitle: "Edit User" });
-export const remove = (req, res) => res.render("remove", { pageTitle: "Remove" });
-export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+export const logout = (req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
+};
 export const see = (req, res) => res.render("see", { pageTitle: "See", userId: `${req.params.id}` });
--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
+    avatarUrl: {type: String},
+    socialOnly: {type : Boolean, default: false},
     username: { type: String, required: true, unique: true },
-    password: { type: String, reuqired: true },
+    password: { type: String, reuqired: false },
     name: { type: String, required: true },
     loction: String,
 });
